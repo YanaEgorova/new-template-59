83782a05df04b5ab0f99709b494a40159222ec5a
Yana <yanaegorova69.3@gmail.com>
2022-03-17 22:37:28 +0200

    +

--- thank-you.php
+++ thank-you.php
@@ -57,8 +57,10 @@
           <div class="container">
               <div class="block">
                   <img src="./img/thank6.png" alt="" class="img">
-                <!-- <h1 class="title">Thank you</h1> -->
                 <p class="text">Your order was completed successfully!</p>
+                <?php if($_GET["order_id"]){ ?> 
+      <p class="text" style="font-size: 3rem">Your Order ID is: <?php echo htmlspecialchars($_GET["order_id"]); ?></p>
+    <?php } ?>
               </div>
           </div>
         </section>
